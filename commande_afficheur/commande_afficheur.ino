f892764b95dc2b9764c84bd5a9e8244bf0280983
LE HOOP <nicolas.guichard@lehoop.fr>
2023-11-09 17:39:26 +0100

    unite et decimal

--- commande_afficheur/commande_afficheur.ino
+++ commande_afficheur/commande_afficheur.ino
@@ -7,10 +7,15 @@
 // $Id: Random.pde,v 1.1 2011/01/05 01:51:01 mikem Exp mikem $
 #include <Conceptinetics.h>
 #include <AccelStepper.h>
-int chiffre=0, chiffre_temp=0;
+int unite=0, unite_temp=0;
+int deci=0, deci_temp=0;
 // Define a stepper and the pins it will use
-AccelStepper stepper(1, 3, 4); // Defaults to AccelStepper::FULL4WIRE (4 pins) on 2, 3, 4, 5
+AccelStepper stepper_unite(1, 3, 4); // Defaults to AccelStepper::FULL4WIRE (4 pins) on 2, 3, 4, 5
+AccelStepper stepper_deci(1, 5, 6);
 int incomingByte;
+
+int unite_sensor_pin=8;
+int deci_sensor_pin=7;
 
 
 #define DMX_SLAVE_CHANNELS   2 
@@ -21,12 +26,14 @@ void setup()
   //Serial.begin(9600);
   // Change these to suit your stepper if you want
 
-  pinMode(8,INPUT);
-  stepper.setMaxSpeed(1600);
-  stepper.setAcceleration(400);
+  pinMode(unite_sensor_pin,INPUT);
+  stepper_unite.setMaxSpeed(1600);
+  stepper_unite.setAcceleration(400);
+  stepper_deci.setMaxSpeed(1600);
+  stepper_deci.setAcceleration(400);
 
 
-    dmx_slave.enable (); 
+  dmx_slave.enable (); 
   dmx_slave.setStartAddress (1);
 }
 
@@ -38,43 +45,68 @@ void loop()
 
     if (incomingByte-48>=0){
     //Serial.println(int(incomingByte)-48);
-    chiffre=int(incomingByte)-48;
-    if (chiffre>chiffre_temp){
-      stepper.moveTo((chiffre-chiffre_temp)*-80);
+    unite=int(incomingByte)-48;
+    if (unite>unite_temp){
+      stepper.moveTo((unite-unite_temp)*-80);
     }
     else{
-stepper.moveTo(((10-chiffre_temp)*-80)+((chiffre)*-80));
+stepper.moveTo(((10-unite_temp)*-80)+((unite)*-80));
 
     }
     }
   }
 */
 
-if ((dmx_slave.getChannelValue (1)/2.55)<100){
+if (round(dmx_slave.getChannelValue (1)/2.55)<100){
 
-chiffre= round(dmx_slave.getChannelValue (1)/2.55);
-    if (chiffre>chiffre_temp){
-      stepper.moveTo((chiffre-chiffre_temp)*-80);
+unite = (round(dmx_slave.getChannelValue (1)/2.55) );
+unite= unite % 10;
+    if (unite>unite_temp){
+      stepper_unite.moveTo((unite-unite_temp)*-80);
     }
-    else if( chiffre<chiffre_temp){
-stepper.moveTo(((10-chiffre_temp)*-80)+((chiffre)*-80));
+    else if( unite<unite_temp){
+stepper_unite.moveTo(((10-unite_temp)*-80)+((unite)*-80));
 
     }
+
+deci = (round(dmx_slave.getChannelValue (1)/2.55) );
+deci= int((deci % 100)/10);
+    if (deci>deci_temp){
+      stepper_deci.moveTo((deci-deci_temp)*-80);
+    }
+    else if( deci<deci_temp){
+stepper_deci.moveTo(((10-deci_temp)*-80)+((deci)*-80));
+
+    }
+
 }
   
     // If at the end of travel go to the other end
-    if (stepper.distanceToGo() == 0){
-      stepper.setCurrentPosition(0);
-      chiffre_temp=chiffre;
-  //    Serial.print("chiffre:");
- //     Serial.print(chiffre);
-   //   Serial.print("    chiffretemp:");
-   //   Serial.println(chiffre_temp);
+    if (stepper_unite.distanceToGo() == 0){
+      stepper_unite.setCurrentPosition(0);
+      unite_temp=unite;
+  //    Serial.print("unite:");
+ //     Serial.print(unite);
+   //   Serial.print("    unitetemp:");
+   //   Serial.println(unite_temp);
      
      }
 
-if(digitalRead(8)==0){
-  chiffre_temp=0;
+
+         // If at the end of travel go to the other end
+    if (stepper_deci.distanceToGo() == 0){
+      stepper_deci.setCurrentPosition(0);
+      deci_temp=deci;
+  //    Serial.print("unite:");
+ //     Serial.print(unite);
+   //   Serial.print("    unitetemp:");
+   //   Serial.println(unite_temp);
+     
+     }
+
+if(digitalRead(unite_sensor_pin)==0){
+  unite_temp=0;
 }
-    stepper.run();
+    stepper_unite.run();
+    stepper_deci.run();
 }
