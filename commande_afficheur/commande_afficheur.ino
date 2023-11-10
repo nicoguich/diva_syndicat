433c46d3ecdfc9e50c772cbdad201910ac6e57fb
LE HOOP <nicolas.guichard@lehoop.fr>
2023-11-10 16:34:45 +0100

    effet_hall ok / offset

--- commande_afficheur/commande_afficheur.ino
+++ commande_afficheur/commande_afficheur.ino
@@ -16,6 +16,8 @@ int incomingByte;
 
 int unite_sensor_pin=8;
 int deci_sensor_pin=7;
+int demarrage_unite=0;
+int offset_sensor=-40;
 
 
 #define DMX_SLAVE_CHANNELS   2 
@@ -27,10 +29,12 @@ void setup()
   // Change these to suit your stepper if you want
 
   pinMode(unite_sensor_pin,INPUT);
+ // stepper_unite.setSpeed(500);
   stepper_unite.setMaxSpeed(1600);
-  stepper_unite.setAcceleration(400);
+  stepper_unite.setAcceleration(800);
   stepper_deci.setMaxSpeed(1600);
-  stepper_deci.setAcceleration(400);
+  //stepper_deci.setSpeed(1600);
+  stepper_deci.setAcceleration(800);
 
 
   dmx_slave.enable (); 
@@ -57,34 +61,56 @@ stepper.moveTo(((10-unite_temp)*-80)+((unite)*-80));
   }
 */
 
+if (dmx_slave.getChannelValue (2)<127){
+
+if (demarrage_unite==0){
+  stepper_unite.moveTo(-5000);
+if(digitalRead(unite_sensor_pin)==1){
+  stepper_unite.run();
+}
+else{
+demarrage_unite=1;
+unite_temp=0;
+stepper_unite.setCurrentPosition(0);
+stepper_unite.moveTo(offset_sensor);}
+  
+}
+
+
+else {
+if (demarrage_unite==2){
 if (round(dmx_slave.getChannelValue (1)/2.55)<100){
 
 unite = (round(dmx_slave.getChannelValue (1)/2.55) );
 unite= unite % 10;
     if (unite>unite_temp){
-      stepper_unite.moveTo((unite-unite_temp)*-80);
+      stepper_unite.moveTo((unite-unite_temp)*-160);
     }
     else if( unite<unite_temp){
-stepper_unite.moveTo(((10-unite_temp)*-80)+((unite)*-80));
+stepper_unite.moveTo(((10-unite_temp)*-160)+((unite)*-160));
 
     }
 
 deci = (round(dmx_slave.getChannelValue (1)/2.55) );
 deci= int((deci % 100)/10);
     if (deci>deci_temp){
-      stepper_deci.moveTo((deci-deci_temp)*-80);
+      stepper_deci.moveTo((deci-deci_temp)*-160);
     }
     else if( deci<deci_temp){
-stepper_deci.moveTo(((10-deci_temp)*-80)+((deci)*-80));
+stepper_deci.moveTo(((10-deci_temp)*-160)+((deci)*-160));
 
     }
 
 }
-  
+}
     // If at the end of travel go to the other end
     if (stepper_unite.distanceToGo() == 0){
       stepper_unite.setCurrentPosition(0);
       unite_temp=unite;
+      if(demarrage_unite==1){
+        demarrage_unite=2;
+unite_temp=0;
+      }
   //    Serial.print("unite:");
  //     Serial.print(unite);
    //   Serial.print("    unitetemp:");
@@ -104,9 +130,23 @@ stepper_deci.moveTo(((10-deci_temp)*-80)+((deci)*-80));
      
      }
 
-if(digitalRead(unite_sensor_pin)==0){
+/*if(digitalRead(unite_sensor_pin)==0){
   unite_temp=0;
+  
 }
+*/
+
+    }
+}
+
+else{
+  
+
+stepper_unite.moveTo(-100000);
+  demarrage_unite=0;
+}
+
+
     stepper_unite.run();
-    stepper_deci.run();
+    stepper_deci.run();   
 }
